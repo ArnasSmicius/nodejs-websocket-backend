a2d96bc204d5311aaa8a7d740521e7b167bded17
Arnas Smicius <arnasmic@gmail.com>
2018-08-18 08:52:33 +0300

    Added web socket

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
 var server = app.listen(process.env.PORT || 4000, function() {
-    console.log('listening to requests on port: 4000')
+    console.log('listening to requests on port: 4000');
 });
 
 // Static files
 app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server)
+
+io.on('connection', function(socket) {
+    console.log('made socket connection', socket.id)
+});
+
+// https://www.youtube.com/watch?v=KNqVpESuyQo&list=PL4cUxeGkcC9i4V-_ZVwLmOusj8YAUhj_9&index=4
